b85bc4f2c9a8c1598431e9162e10b569e88405ea
Sokiyna <sokiynanaser@yahoo.com>
2021-07-15 18:04:22 +0300

    add db

--- index.js
+++ index.js
@@ -5,11 +5,48 @@ const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 const server = http.createServer(app);
 const io = require('socket.io')(http);
-const staffRoom = 'staff';
+const adminroom = 'adminroom';
 const { v4: uuidv4 } = require('uuid');
 const faker = require('faker');
+const mongoose = require('mongoose');
 
 io.listen(server);
+
+const queue = {
+  tickets: [],
+  staff: [],
+};
+
+mongoose.connect('mongodb://localhost:27017/ordars', { useNewUrlParser: true, useUnifiedTopology: true });
+const orderSchema = new mongoose.Schema({
+  clientName: String,
+  address: String,
+  phone: String,
+  Airlines:String,
+  id:String,
+});
+const orderModel = mongoose.model('ordars', orderSchema);
+
+class Order {
+  constructor(tickets) {
+    this. clientName = tickets. clientName;
+    this.address = tickets.address;
+    this.Airlines = tickets.Airlines;
+    this.id = tickets.id;
+
+  }
+}
+app.get('/admin', getOrderHandler);
+
+
+function getOrderHandler(req, res) {
+  console.log(queue.tickets)
+  const dataTickets = queue.tickets.map(element => {
+    return new Order(element);
+  })
+  res.send(dataTickets);
+  console.log(dataTickets);
+}
 
 app.use(cors());
 // app.get('/hi', (req, res) => {
@@ -20,28 +57,68 @@ io.on('connection', (socket) => {
   // console.log('clie.nt connected', socket.id);
   //2a
   socket.on('join', (payload) => {
-    // socket.join will put the socket in a private room
-    socket.join(staffRoom);
-    socket.to(staffRoom).emit('onlineStaff', { name: payload.name, id: socket.id });
+    socket.join(adminroom);
   });
-  socket.on('createTicket', (payload) => {
-    // 2
+  socket.on('sendRequest', (payload) => {
+    const flightData = { ...payload, id: uuidv4(), socketId: socket.id };
+    queue.tickets.push(flightData);
+    console.log(flightData);
     socket
-      .in(staffRoom)
-      .emit('newTicket', { ...payload, id: uuidv4(), socketId: socket.id });
+      .in(adminroom)
+      .emit('newReq', { ...payload, id: uuidv4(), socketId: socket.id });
   });
+  socket.on('sendmsg', (payload) => {
+    // console.log("test"+JSON.stringify(payload))
+    socket
+    .in(adminroom)
+.emit('sentMsg', { ...payload, id: uuidv4(), socketId:socket.id});
+// console.log(payload)
+  });
+  //sendmsg
 
-  socket.on('claim', (payload) => {
-    // when a TA claim the ticket we need to notify the student
+// socket.on('replay', (payload) => {
+
+//   socket
+//     .to(payload.id)
+//     .emit('replaymsg', {msg:payload.msg,time:payload.created_at});
+// });
+  socket.on('handle', (payload) => {
+    // console.log(payload.flag)
+    if(payload.flag==true)
+    {
     socket.to(payload.studentId).emit('claimed', { name: payload.name,
-        randomNum : faker.date.future()
-    
+        randomNum : faker.date.future().toLocaleDateString(),
+    txt:"Your jorney has been accepted it will be in"
     });
-
-  
+  }
+  else
+  {
+    socket.to(payload.studentId).emit('claimed', { name: payload.name,
+      randomNum : '',
+  txt:"Sorry Your jorney hasn't been accepted" 
   });
+  }
+  });
+
+
+  socket.on('replayTo', (payload) => {
+
+//  socket.to(payload.studentId).emit('claimed', { name: payload.name,
+//       randomNum : '',
+//   txt:"Sorry Your jorney hasn't been accepted" 
+//   });
+//   }
+    socket.broadcast.emit('replaiedtoUsers', payload);
+
+  });
+
+
+
+
   socket.on('disconnect', () => {
-    socket.to(staffRoom).emit('offlineStaff', { id: socket.id });
+    socket.to(adminroom).emit('offlineStaff', { id: socket.id });
+
+    console.log("offline")
   });
 });
 server.listen(PORT, () => {
