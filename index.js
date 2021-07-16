3bcebef1086cbb527893a40ee27742f083ffe721
Sokiyna <sokiynanaser@yahoo.com>
2021-07-16 18:53:13 +0300

    deploy

--- index.js
+++ index.js
@@ -8,152 +8,92 @@ const io = require('socket.io')(http);
 const adminroom = 'adminroom';
 const { v4: uuidv4 } = require('uuid');
 const faker = require('faker');
-let username=""
+let username = ""
 const mongoose = require('mongoose');
-let num=1;
+let num = 1;
 io.listen(server);
-
-const queue = {
-  tickets: [],
-  staff: [],
-};
-
-mongoose.connect('mongodb://localhost:27017/ordars', { useNewUrlParser: true, useUnifiedTopology: true });
-const orderSchema = new mongoose.Schema({
-  clientName: String,
-  address: String,
-  phone: String,
-  Airlines:String,
-  id:String,
-});
-const orderModel = mongoose.model('ordars', orderSchema);
-
-class Order {
-  constructor(tickets) {
-    this. clientName = tickets. clientName;
-    this.address = tickets.address;
-    this.Airlines = tickets.Airlines;
-    this.id = tickets.id;
-
-  }
-}
-app.get('/admin', getOrderHandler);
-
-
-function getOrderHandler(req, res) {
-  console.log(queue.tickets)
-  const dataTickets = queue.tickets.map(element => {
-    return new Order(element);
-  })
-  res.send(dataTickets);
-  console.log(dataTickets);
-}
-
 app.use(cors());
-
 mongoose.connect('mongodb://localhost:27017/flights', { useNewUrlParser: true, useUnifiedTopology: true });
+process.env.MONGODB_URI
 const orderSchema = new mongoose.Schema({
   clientName: String,
   address: String,
   phone: String,
-  Airlines:String,
-  counter2:String
+  Airlines: String,
+  counter2: String
 });
 const orderModel = mongoose.model('ordars', orderSchema);
-
-
 io.on('connection', (socket) => {
   // console.log('clie.nt connected', socket.id);
   //2a
   socket.on('join', (payload) => {
     socket.join(adminroom);
   });
-  
-  
-  
-  
-  
-  
-
-  socket.on('sendRequest', (payload) => {
-
+  socket.on('sendRequest', async (payload) => {
+    payload.counter2 = num++;
+    let tst = new Order(payload)
+    let newtic = await new orderModel(tst)
+    newtic.save();
+    console.log(newtic)
     socket
       .in(adminroom)
       .emit('newReq', { ...payload, id: uuidv4(), socketId: socket.id });
   });
   socket.on('sendmsg', (payload) => {
     // console.log("test"+JSON.stringify(payload))
-
+    username = payload.userName
+    console.log(payload)
     socket
-    .in(adminroom)
-.emit('sentMsg', { ...payload, id: uuidv4(), socketId:socket.id});
-// console.log(payload)
+      .in(adminroom)
+      .emit('sentMsg', { ...payload, id: uuidv4(), socketId: socket.id });
+    // console.log(payload)
   });
-  //sendmsg
-
-
-
-
-  socket.on('getAllQueuing', async() => {
-  
-    const allData= await orderModel.find({}).then(function (orders) {
-return orders
-      });
-
+  socket.on('getAllQueuing', async () => {
+    const allData = await orderModel.find({}).then(function (orders) {
+      return orders
+    });
     allData.forEach((data) => {
-        socket.emit('newReq', data);
-      });
-
-
-
-      console.log(allData+"tresdA")
-  
+      socket.emit('newReq', data);
+    });
+    console.log(allData + "tresdA")
   });
-
   socket.on('handle', (payload) => {
     // console.log(payload.flag)
-    if(payload.flag==true)
-    {
-
-        randomNum : faker.date.future().toLocaleDateString(),
-    txt:"Your jorney has been accepted it will be in"
-    });
-  }
-  else
-  {
-
-  txt:"Sorry Your jorney hasn't been accepted" 
+    if (payload.flag == true) {
+      socket.to(payload.studentId).emit('results', {
+        name: payload.name,
+        flag2: true,
+        randomNum: faker.date.future().toLocaleDateString(),
+        txt: "Your jorney has been accepted it will be in"
+      });
+    }
+    else {
+      socket.to(payload.studentId).emit('results', {
+        name: payload.name,
+        flag2: false,
+        txt: "Sorry Your jorney hasn't been accepted"
+      });
+    }
   });
-  }
-  });
-
-
   socket.on('replayTo', (payload) => {
-
-
-
-console.log(payload)
-    socket.broadcast.emit('replaiedtoUsers', payload,username);
-//payload.userId
-
+    console.log(payload)
+    socket.broadcast.emit('replaiedtoUsers', payload, username);
+    //payload.userId
   });
   socket.on('disconnect', () => {
     socket.to(adminroom).emit('offlineStaff', { id: socket.id });
-
     console.log("offline")
   });
 });
 server.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
 });
-
 class Order {
   constructor(tickets) {
-    this. clientName = tickets. clientName;
+    this.clientName = tickets.clientName;
     this.address = tickets.address;
     this.Airlines = tickets.Airlines;
-this.phone=tickets.phone
-this.counter2=tickets.counter2
-
+    this.phone = tickets.phone
+    this.counter2 = tickets.counter2
   }
 }
