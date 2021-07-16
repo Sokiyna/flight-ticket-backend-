aeaa655b3bbc40fc32aaabc94443b4b9ec3fee1c
bahaamq <bahaa.mq@gmail.com>
2021-07-16 17:12:52 +0300

    lastupdates

--- index.js
+++ index.js
@@ -9,12 +9,22 @@ const adminroom = 'adminroom';
 const { v4: uuidv4 } = require('uuid');
 const faker = require('faker');
 let username=""
+const mongoose = require('mongoose');
+let num=1;
 io.listen(server);
 
 app.use(cors());
-// app.get('/hi', (req, res) => {
-//   res.send('Hello World');
-// });
+
+mongoose.connect('mongodb://localhost:27017/flights', { useNewUrlParser: true, useUnifiedTopology: true });
+const orderSchema = new mongoose.Schema({
+  clientName: String,
+  address: String,
+  phone: String,
+  Airlines:String,
+  counter2:String
+});
+const orderModel = mongoose.model('ordars', orderSchema);
+
 
 io.on('connection', (socket) => {
   // console.log('clie.nt connected', socket.id);
@@ -22,7 +32,13 @@ io.on('connection', (socket) => {
   socket.on('join', (payload) => {
     socket.join(adminroom);
   });
-  socket.on('sendRequest', (payload) => {
+  socket.on('sendRequest', async(payload) => {
+    payload.counter2=num++;
+let tst= new Order(payload)
+    let newtic= await new orderModel(tst)
+    newtic.save();
+
+    console.log(newtic)
     socket
       .in(adminroom)
       .emit('newReq', { ...payload, id: uuidv4(), socketId: socket.id });
@@ -35,6 +51,24 @@ io.on('connection', (socket) => {
     .in(adminroom)
 .emit('sentMsg', { ...payload, id: uuidv4(), socketId:socket.id});
 // console.log(payload)
+  });
+
+
+
+  socket.on('getAllQueuing', async() => {
+  
+    const allData= await orderModel.find({}).then(function (orders) {
+return orders
+      });
+
+    allData.forEach((data) => {
+        socket.emit('newReq', data);
+      });
+
+
+
+      console.log(allData+"tresdA")
+  
   });
 
   socket.on('handle', (payload) => {
@@ -59,20 +93,11 @@ io.on('connection', (socket) => {
 
   socket.on('replayTo', (payload) => {
 
-//  socket.to(payload.studentId).emit('results', { name: payload.name,
-//       randomNum : '',
-//   txt:"Sorry Your jorney hasn't been accepted" 
-//   });
-//   }
 
 console.log(payload)
     socket.broadcast.emit('replaiedtoUsers', payload,username);
 //payload.userId
   });
-
-
-
-
   socket.on('disconnect', () => {
     socket.to(adminroom).emit('offlineStaff', { id: socket.id });
 
@@ -82,3 +107,14 @@ console.log(payload)
 server.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
 });
+
+class Order {
+  constructor(tickets) {
+    this. clientName = tickets. clientName;
+    this.address = tickets.address;
+    this.Airlines = tickets.Airlines;
+this.phone=tickets.phone
+this.counter2=tickets.counter2
+
+  }
+}
